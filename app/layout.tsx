e5842c995148fbe1279ebaf442220d44e27621b8
Fervicmar Lagman <fervicmardlagman@gmail.com>
2026-06-30 23:55:22 +0800

    feat(page): update layout and individual page file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
-import { Bricolage_Grotesque, JetBrains_Mono, Onest } from "next/font/google";
+import {
+	Bricolage_Grotesque,
+	Jersey_10,
+	JetBrains_Mono,
+	Onest,
+} from "next/font/google";
 
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
@@ -8,43 +13,58 @@ import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
 
 const onest = Onest({
-    subsets: ["latin"],
-    variable: "--font-onest",
+	subsets: ["latin"],
+	variable: "--font-onest",
 });
 
 const bricolage = Bricolage_Grotesque({
-    subsets: ["latin"],
-    variable: "--font-bricolage",
+	subsets: ["latin"],
+	variable: "--font-bricolage",
 });
 
-const jetbrainsMono = JetBrains_Mono({ subsets: ['latin'], variable: '--font-mono' });
+const jersey = Jersey_10({
+	subsets: ["latin"],
+	variable: "--font-jersey-10",
+	weight: "400",
+});
+
+const jetbrainsMono = JetBrains_Mono({
+	subsets: ["latin"],
+	variable: "--font-mono",
+});
 
 export const metadata: Metadata = {
-    title: {
-        default: "Fervicmar Portfolio",
-        template: "%s | Fervicmar Portfolio",
-    },
-    description: "Portfolio of Fervicmar",
+	title: {
+		default: "Fervicmar Portfolio",
+		template: "%s | Fervicmar Portfolio",
+	},
+	description: "Portfolio of Fervicmar",
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-    return (
-        <html
-            lang="en"
-            className={cn(onest.variable, bricolage.variable, "font-mono", jetbrainsMono.variable)}
-        >
-            <body className=" text-black antialiased">
-                <div className="flex min-h-screen flex-col">
-                    <Header />
-                    <main className="flex-1">{children}</main>
-                    <Footer />
-                    <Toaster richColors position="top-right" />
-                </div>
-            </body>
-        </html>
-    );
+	return (
+		<html
+			lang="en"
+			className={cn(
+				onest.variable,
+				bricolage.variable,
+				jersey.variable,
+				jetbrainsMono.variable,
+				"font-mono",
+			)}
+		>
+			<body className=" text-black antialiased">
+				<div className="flex min-h-screen flex-col">
+					<Header />
+					<main className="flex-1">{children}</main>
+					<Footer />
+					<Toaster />
+				</div>
+			</body>
+		</html>
+	);
 }
